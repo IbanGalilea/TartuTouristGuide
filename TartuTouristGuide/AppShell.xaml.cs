643a683c26d456a83f261a0c38fc2da88b2f690c
Ibande <iban.galilea@gmail.com>
2026-02-10 13:46:48 +0200

    first commit

---
+++ TartuTouristGuide/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace TartuTouristGuide
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ TartuTouristGuide/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "TartuTouristGuide")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "TartuTouristGuide.Pages")]
