dee137863c9f7ab2da145b9ca6ecd72cb8119e6d
Ibande <iban.galilea@gmail.com>
2026-02-11 15:01:42 +0200

    fait chier

---
+++ TartuTouristGuide/Views/Tartu101Page.xaml.cs
@@ -0,0 +1,29 @@
+using TartuTouristGuide.ViewModels;
+
+namespace TartuTouristGuide.Views
+{
+    public partial class Tartu101Page : ContentPage
+    {
+        public Tartu101Page()
+        {
+            InitializeComponent();
+        }
+
+        private async void OnWebsiteTapped(object sender, EventArgs e)
+        {
+            try
+            {
+                await Browser.OpenAsync("https://visittartu.com", BrowserLaunchMode.SystemPreferred);
+            }
+            catch
+            {
+                // Handle error silently
+            }
+        }
+
+        private async void OnBackClicked(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync("..");
+        }
+    }
+}
